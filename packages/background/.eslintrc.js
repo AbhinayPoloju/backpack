77c9bd4801309206b35fe4266da7f69c552723e8
AbhinayPoloju <84982230+AbhinayPoloju@users.noreply.github.com>
2023-10-07 13:25:14 +0530

    Update .eslintrc.js

--- packages/background/.eslintrc.js
+++ packages/background/.eslintrc.js
@@ -1,8 +1,16 @@
 module.exports = {
+  // Indicates that this is the root ESLint configuration for your project.
   root: true,
+  
+  // Extends a custom ESLint configuration named "custom."
   extends: ["custom"],
+  
+  // Specifies options related to TypeScript parsing.
   parserOptions: {
+    // Sets the directory where ESLint should look for tsconfig.json.
     tsconfigRootDir: __dirname,
+    
+    // Specifies the path to your TypeScript project configuration file.
     project: ["./tsconfig.json"],
   },
 };
